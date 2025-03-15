d3b36b4605fe26670cfd79a3bc737fa26de14824
Anderson Paulo <andersonpauloti@gmail.com>
2025-03-15 19:36:18 -0300

    Add atributos data-qa

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import './Display.css'
 
-const props = props => <div className="display">{props.value}</div>
+const props = props => <div data-qa="display" className="display">{props.value}</div>
 
 export default props 
    
